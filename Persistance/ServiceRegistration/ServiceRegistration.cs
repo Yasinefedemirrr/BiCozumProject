a5343adc6f387ebde2a9ae936f338f0a4641959b
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:39:12 +0300

    ServiceRegistration

---
+++ Persistance/ServiceRegistration/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Application.interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.ServiceRegistration
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IComplaintRepository, ComplaintRepository>();
+            services.AddScoped<IAssignmentRepository, AssignmentRepository>();
+            services.AddScoped<IComplaintHistoryRepository, ComplaintHistoryRepository>();
+
+            return services;
+        }
+    }
+}
