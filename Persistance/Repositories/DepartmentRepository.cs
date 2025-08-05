9d0cbf7061e6da5879e6ae7df63e25be645fb19a
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:38:38 +0300

    Repository

---
+++ Persistance/Repositories/DepartmentRepository.cs
@@ -0,0 +1,27 @@
+﻿using Application.interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class DepartmentRepository : Repository<Department>, IDepartmentRepository
+    {
+        private readonly BiCozumContext _context;
+
+        public DepartmentRepository(BiCozumContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Department?> GetByNameAsync(string name)
+        {
+            return await _context.Departments.FirstOrDefaultAsync(d => d.Name == name);
+        }
+    }
+}
