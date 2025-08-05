3cc391b9df3eab3ba3ceb1232f7a9851a9a8f2d2
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 12:29:29 +0300

    Context

--- Persistance/Context/BiCozumContext.cs
+++ Persistance/Context/BiCozumContext.cs
@@ -20,7 +20,47 @@ namespace Persistance.Context
         {
             base.OnModelCreating(modelBuilder);
 
-            // İleride Fluent API ayarlarını buraya ekleyeceğiz
+            // User -> Department (1-N)
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Department)
+                .WithMany(d => d.Users)
+                .HasForeignKey(u => u.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Complaint -> User (1-N, Vatandaş)
+            modelBuilder.Entity<Complaint>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Complaints)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Complaint -> Department (1-N)
+            modelBuilder.Entity<Complaint>()
+                .HasOne(c => c.Department)
+                .WithMany(d => d.Complaints)
+                .HasForeignKey(c => c.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Assignment -> Complaint (1-N) - Cascade kapalı
+            modelBuilder.Entity<Assignment>()
+                .HasOne(a => a.Complaint)
+                .WithMany(c => c.Assignments)
+                .HasForeignKey(a => a.ComplaintId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Assignment -> User (1-N, Personel) - Cascade kapalı
+            modelBuilder.Entity<Assignment>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Assignments)
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // ComplaintHistory -> Complaint (1-N) - Cascade kapalı
+            modelBuilder.Entity<ComplaintHistory>()
+                .HasOne(ch => ch.Complaint)
+                .WithMany(c => c.ComplaintHistories)
+                .HasForeignKey(ch => ch.ComplaintId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
---
+++ Persistance/Context/BiCozumContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Context
+{
+    public class BiCozumContextFactory : IDesignTimeDbContextFactory<BiCozumContext>
+    {
+        public BiCozumContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BiCozumContext>();
+
+            // Connection string
+            optionsBuilder.UseSqlServer("Server=YASINEFEDEMIR\\SQLEXPRESS;Database=BiCozum;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new BiCozumContext(optionsBuilder.Options);
+        }
+    }
+}
