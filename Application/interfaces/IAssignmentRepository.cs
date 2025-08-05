2fa6db7a4d465219894c3ab64bf8bcf4fa7e506e
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:36:50 +0300

    interfaces

---
+++ Application/interfaces/IAssignmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.interfaces
+{
+    public interface IAssignmentRepository : IRepository<Assignment>
+    {
+        Task<List<Assignment>> GetByComplaintAsync(int complaintId);
+        Task<List<Assignment>> GetByUserAsync(int userId);
+    }
+}
