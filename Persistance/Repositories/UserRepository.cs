0d1b26516cae1f57951c4f1a15a017942536c057
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:38:53 +0300

    Repository

---
+++ Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Application.interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly BiCozumContext _context;
+
+        public UserRepository(BiCozumContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetUsersByDepartmentAsync(int departmentId)
+        {
+            return await _context.Users
+                .Where(u => u.DepartmentId == departmentId)
+                .ToListAsync();
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+    }
+
+}
