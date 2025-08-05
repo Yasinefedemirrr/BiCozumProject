273e35932505cda89e5c79c073e28c3b101b344f
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:37:16 +0300

    interfaces

---
+++ Application/interfaces/IComplaintRepository.cs
@@ -0,0 +1,16 @@
+﻿using Application.interfaces;
+using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.interfaces
+{
+    public interface IComplaintRepository : IRepository<Complaint>
+    {
+        Task<List<Complaint>> GetByDepartmentAsync(int departmentId);
+        Task<List<Complaint>> GetByUserAsync(int userId);
+    }
+}
---
+++ Application/interfaces/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.interfaces
+{
+    public interface IDepartmentRepository : IRepository<Department>
+    {
+        Task<Department?> GetByNameAsync(string name);
+    }
+}
