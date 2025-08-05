34fd602231b5fea62c357a5ad800b78d1e3c49d9
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:37:37 +0300

    interfaces

---
+++ Application/interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<List<User>> GetUsersByDepartmentAsync(int departmentId);
+        Task<User?> GetByEmailAsync(string email);
+    }
+}
