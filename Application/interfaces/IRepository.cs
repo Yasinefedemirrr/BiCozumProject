1e9bde2ae8d8df98730a17b9bfbf90aac6397a80
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:37:28 +0300

    interfaces

---
+++ Application/interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    }
+}
