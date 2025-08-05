9a81f739722b06bea867fa8014f75498eb71a43c
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:38:08 +0300

    Repository

---
+++ Persistance/Repositories/AssignmentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Application.interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class AssignmentRepository : Repository<Assignment>, IAssignmentRepository
+    {
+        private readonly BiCozumContext _context;
+
+        public AssignmentRepository(BiCozumContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Assignment>> GetByComplaintAsync(int complaintId)
+        {
+            return await _context.Assignments
+                .Where(a => a.ComplaintId == complaintId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Assignment>> GetByUserAsync(int userId)
+        {
+            return await _context.Assignments
+                .Where(a => a.UserId == userId)
+                .ToListAsync();
+        }
+    }
+}
