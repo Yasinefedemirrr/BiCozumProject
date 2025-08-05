5d8c430faae06ac92f35370e79f23d48e5c0133a
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 13:38:29 +0300

    Repository

---
+++ Persistance/Repositories/ComplaintRepository.cs
@@ -0,0 +1,34 @@
+﻿using Application.interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class ComplaintRepository : Repository<Complaint>, IComplaintRepository
+    {
+        private readonly BiCozumContext _context;
+
+        public ComplaintRepository(BiCozumContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Complaint>> GetByDepartmentAsync(int departmentId)
+        {
+            return await _context.Complaints
+                .Where(c => c.DepartmentId == departmentId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Complaint>> GetByUserAsync(int userId)
+        {
+            return await _context.Complaints
+                .Where(c => c.UserId == userId)
+                .ToListAsync();
+        }
+    }
+}
