967dabb40484ffbe178064b235f02be7c58b51c9
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:32:47 +0300

    Assignment Command

---
+++ Application/Features/Commands/AssignmentCommands/CreateAssignmentCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Commands.AssignmentCommands
+{
+    public class CreateAssignmentCommand : IRequest<int>
+    {
+        public int ComplaintId { get; set; }
+        public int UserId { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.Now;
+        public string Progress { get; set; } = "Beklemede";
+    }
+}
