6ca6151d7df403d72074ff7efcc703d168702666
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 11:38:18 +0300

    Migrations

---
+++ Persistance/Migrations/20250806083137_mig_cascade_user_complaints.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_cascade_user_complaints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Complaints_Users_UserId",
+                table: "Complaints");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Complaints_Users_UserId",
+                table: "Complaints",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Complaints_Users_UserId",
+                table: "Complaints");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Complaints_Users_UserId",
+                table: "Complaints",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
