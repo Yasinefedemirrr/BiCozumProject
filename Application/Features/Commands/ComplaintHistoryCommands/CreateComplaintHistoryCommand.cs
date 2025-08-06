50aad95e737ccadc20478ab6f224e4b5cc9a3bd0
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:38:05 +0300

    ComplaintHistory Command

---
+++ Application/Features/Commands/ComplaintHistoryCommands/CreateComplaintHistoryCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Commands.ComplaintHistoryCommands
+{
+    public class CreateComplaintHistoryCommand : IRequest<int>
+    {
+        public int ComplaintId { get; set; }
+        public string Status { get; set; }
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+
+}
