cf1f051d3ffde492f222c9fc1557644dcfd6ce7e
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 11:37:41 +0300

    User interfaces

--- Application/interfaces/IUserRepository.cs
+++ Application/interfaces/IUserRepository.cs
@@ -11,5 +11,7 @@ namespace Application.interfaces
     {
         Task<List<User>> GetUsersByDepartmentAsync(int departmentId);
         Task<User?> GetByEmailAsync(string email);
+        Task<List<User>> GetAllWithIncludesAsync();
+        Task<User?> GetByIdWithIncludesAsync(int id);
     }
 }
