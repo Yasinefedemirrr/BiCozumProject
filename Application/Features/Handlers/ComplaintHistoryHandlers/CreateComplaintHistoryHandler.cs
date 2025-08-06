c8afdf91eab1b3048b15c3daf43150ff546677f8
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:38:42 +0300

    ComplaintHistoryHandler

---
+++ Application/Features/Handlers/ComplaintHistoryHandlers/CreateComplaintHistoryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.Commands.ComplaintHistoryCommands;
+using Application.interfaces;
+using Domain.Entity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Handlers.ComplaintHistoryHandlers
+{
+    public class CreateComplaintHistoryHandler : IRequestHandler<CreateComplaintHistoryCommand, int>
+    {
+        private readonly IComplaintHistoryRepository _complaintHistoryRepository;
+
+        public CreateComplaintHistoryHandler(IComplaintHistoryRepository complaintHistoryRepository)
+        {
+            _complaintHistoryRepository = complaintHistoryRepository;
+        }
+
+        public async Task<int> Handle(CreateComplaintHistoryCommand request, CancellationToken cancellationToken)
+        {
+            var history = new ComplaintHistory
+            {
+                ComplaintId = request.ComplaintId,
+                Status = request.Status,
+                Note = request.Note,
+                Date = request.Date
+            };
+
+            await _complaintHistoryRepository.AddAsync(history);
+            return history.Id;
+        }
+    }
+
+}
---
+++ Application/Features/Handlers/ComplaintHistoryHandlers/DeleteComplaintHistoryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Commands.ComplaintHistoryCommands;
+using Application.interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Handlers.ComplaintHistoryHandlers
+{
+    public class DeleteComplaintHistoryHandler : IRequestHandler<DeleteComplaintHistoryCommand, Unit>
+    {
+        private readonly IComplaintHistoryRepository _complaintHistoryRepository;
+
+        public DeleteComplaintHistoryHandler(IComplaintHistoryRepository complaintHistoryRepository)
+        {
+            _complaintHistoryRepository = complaintHistoryRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteComplaintHistoryCommand request, CancellationToken cancellationToken)
+        {
+            var history = await _complaintHistoryRepository.GetByIdAsync(request.Id);
+            if (history != null)
+            {
+                await _complaintHistoryRepository.DeleteAsync(history);
+            }
+            return Unit.Value;
+        }
+    }
+
+}
