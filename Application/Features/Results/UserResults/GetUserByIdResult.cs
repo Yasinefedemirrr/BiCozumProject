0d869c92fc48acee915ec3e35643e08405b3e0b0
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 11:38:37 +0300

    User Results

---
+++ Application/Features/Results/UserResults/GetUserByIdResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Results.UserResults
+{
+    public class GetUserByIdResult
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+        public string DepartmentName { get; set; } = "";
+    }
+}
