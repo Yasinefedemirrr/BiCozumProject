62cb97f20b0d7bfe260e27f4c2bfbb6aea3aac30
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:30:42 +0300

    Assignment result

---
+++ Application/Features/Results/AssignmentResults/GetAllAssignmentsResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Results.AssignmentResults
+{
+    public class GetAllAssignmentsResult
+    {
+        public int Id { get; set; }
+        public DateTime AssignedAt { get; set; }
+        public string Progress { get; set; }
+        public int ComplaintId { get; set; }
+        public string ComplaintTitle { get; set; }
+        public int UserId { get; set; }
+        public string UserFullName { get; set; }
+    }
+}
