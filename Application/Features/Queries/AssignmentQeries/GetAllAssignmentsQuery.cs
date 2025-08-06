65a8cba75169eb2fdea1c332e6004839bfaf69c6
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:31:14 +0300

    Assignment query

---
+++ Application/Features/Queries/AssignmentQeries/GetAllAssignmentsQuery.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Results.AssignmentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.AssignmentQeries
+{
+    public class GetAllAssignmentsQuery : IRequest<List<GetAllAssignmentsResult>> 
+    {
+    }
+
+    
+}
---
+++ Application/Features/Queries/AssignmentQeries/GetAssignmentByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Results.AssignmentResults;
+using Application.Features.Results.ComplaintResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.AssignmentQeries
+{
+    public class GetAssignmentByIdQuery : IRequest<GetAssignmentByIdResult>
+    {
+        public int Id { get; set; }
+        public GetAssignmentByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
