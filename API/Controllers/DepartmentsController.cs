31fa6bb66e80e89948262c917a7c19a13539a8c0
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 12:13:50 +0300

    Department Controller

---
+++ API/Controllers/DepartmentsController.cs
@@ -0,0 +1,69 @@
+﻿using Application.Features.Commands.DepartmentCommands;
+using Application.Features.Queries.DepartmentQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DepartmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllDepartmentsQuery());
+            return Ok(result);
+        }
+
+        // GET: api/Departments/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetDepartmentByIdQuery(id));
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        // POST: api/Departments
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateDepartmentCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            var createdDepartment = await _mediator.Send(new GetDepartmentByIdQuery(id));
+
+            return CreatedAtAction(nameof(GetById), new { id }, createdDepartment);
+        }
+
+        // PUT: api/Departments/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateDepartmentCommand command)
+        {
+            if (id != command.Id)
+                return BadRequest();
+
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // DELETE: api/Departments/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteDepartmentCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
