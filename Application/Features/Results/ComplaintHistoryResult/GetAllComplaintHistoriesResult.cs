4ae883c6600d0df70abe69676067d230b216f58f
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:35:18 +0300

    Complaint result

---
+++ Application/Features/Results/ComplaintHistoryResult/GetAllComplaintHistoriesResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Results.ComplaintHistoryResult
+{
+    public class GetAllComplaintHistoriesResult
+    {
+        public int Id { get; set; }
+        public int ComplaintId { get; set; }
+        public string ComplaintTitle { get; set; }
+        public string Status { get; set; }
+        public string? Note { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
