e73a3eb2a7e73a7f3de272d8651568e08d3439b8
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 10:18:04 +0300

    Complaint Handlers

---
+++ Application/Features/Handlers/ComplaintHandlers/GetComplaintByIdHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Features.Queries.ComplaintQueries;
+using Application.Features.Results.ComplaintResults;
+using Application.interfaces;
+using MediatR;
+
+
+namespace Application.Features.Handlers.ComplaintHandlers
+{
+    public class GetComplaintByIdHandler : IRequestHandler<GetComplaintByIdQuery, GetComplaintByIdResult>
+    {
+        private readonly IComplaintRepository _complaintRepository;
+
+        public GetComplaintByIdHandler(IComplaintRepository complaintRepository)
+        {
+            _complaintRepository = complaintRepository;
+        }
+
+        public async Task<GetComplaintByIdResult> Handle(GetComplaintByIdQuery request, CancellationToken cancellationToken)
+        {
+            var complaint = await _complaintRepository.GetByIdWithIncludesAsync(request.Id);
+
+            if (complaint == null) return null;
+
+            return new GetComplaintByIdResult
+            {
+                Id = complaint.Id,
+                Title = complaint.Title,
+                Description = complaint.Description,
+                DepartmentName = complaint.Department?.Name ?? "",
+                UserFullName = complaint.User != null ? $"{complaint.User.FullName}" : "",
+                Status = complaint.Status,
+                CreatedAt = complaint.CreatedAt
+            };
+        }
+    }
+}
---
+++ Application/Features/Handlers/ComplaintHandlers/UpdateComplaintHandler.cs
@@ -0,0 +1,38 @@
+﻿using Application.Features.Commands.ComplaintCommands;
+using Application.interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Handlers.ComplaintHandlers
+{
+    public class UpdateComplaintHandler : IRequestHandler<UpdateComplaintCommand, Unit>
+    {
+        private readonly IComplaintRepository _complaintRepository;
+
+        public UpdateComplaintHandler(IComplaintRepository complaintRepository)
+        {
+            _complaintRepository = complaintRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateComplaintCommand request, CancellationToken cancellationToken)
+        {
+            var complaint = await _complaintRepository.GetByIdAsync(request.Id);
+            if (complaint != null)
+            {
+                complaint.Title = request.Title;
+                complaint.Description = request.Description;
+                complaint.DepartmentId = request.DepartmentId;
+                complaint.UserId = request.UserId;
+                complaint.Status = request.Status;
+
+                await _complaintRepository.UpdateAsync(complaint);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
