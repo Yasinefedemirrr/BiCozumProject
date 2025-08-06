e952d8e58d443db1bc0a3014097b0e314f491dc7
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:37:25 +0300

    ComplaintHistory Query

---
+++ Application/Features/Queries/ComplaintHistoryQueries/GetAllComplaintHistoriesQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Results.ComplaintHistoryResult;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.ComplaintHistoryQueries
+{
+    public class GetAllComplaintHistoriesQuery : IRequest<List<GetAllComplaintHistoriesResult>> { }
+
+   
+}
---
+++ Application/Features/Queries/ComplaintHistoryQueries/GetComplaintHistoryByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Results.ComplaintHistoryResult;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.ComplaintHistoryQueries
+{
+    public class GetComplaintHistoryByIdQuery : IRequest<GetComplaintHistoryByIdResult>
+    {
+        public int Id { get; set; }
+        public GetComplaintHistoryByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
