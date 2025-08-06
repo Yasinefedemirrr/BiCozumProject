f323fc402a855fbe258b955c8059b470e32a9efd
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 15:34:06 +0300

    Assignment Controller

---
+++ API/Controllers/AssignmentsController.cs
@@ -0,0 +1,62 @@
+﻿using Application.Features.Commands.AssignmentCommands;
+using Application.Features.Queries.AssignmentQeries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AssignmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllAssignmentsQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetAssignmentByIdQuery(id));
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateAssignmentCommand command)
+        {
+            var id = await _mediator.Send(command);
+            var createdItem = await _mediator.Send(new GetAssignmentByIdQuery(id));
+            return CreatedAtAction(nameof(GetById), new { id }, createdItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateAssignmentCommand command)
+        {
+            if (id != command.Id)
+                return BadRequest();
+
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteAssignmentCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
