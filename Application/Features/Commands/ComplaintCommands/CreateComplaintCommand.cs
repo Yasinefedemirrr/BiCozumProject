5e48d62c33b9f429dc7f7a6a76344d842e9da813
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 10:17:34 +0300

    Complaint Commands

---
+++ Application/Features/Commands/ComplaintCommands/CreateComplaintCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Commands.ComplaintCommands
+{
+    public class CreateComplaintCommand :IRequest<int>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int DepartmentId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Application/Features/Commands/ComplaintCommands/UpdateComplaintCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Commands.ComplaintCommands
+{
+    public class UpdateComplaintCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int DepartmentId { get; set; }
+        public int UserId { get; set; }
+        public string Status { get; set; } = null!;
+    }
+}
