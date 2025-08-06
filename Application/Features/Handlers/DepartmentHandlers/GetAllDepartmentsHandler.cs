ee5bcc090751fc779b8d3d1935cb6e47ed2fe77c
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 12:11:47 +0300

    Department Handlers

---
+++ Application/Features/Handlers/DepartmentHandlers/GetAllDepartmentsHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.Queries.DepartmentQueries;
+using Application.Features.Results.DepartmentResults;
+using Application.interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Handlers.DepartmentHandlers
+{
+    public class GetAllDepartmentsHandler : IRequestHandler<GetAllDepartmentsQuery, List<GetAllDepartmentsResult>>
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public GetAllDepartmentsHandler(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public async Task<List<GetAllDepartmentsResult>> Handle(GetAllDepartmentsQuery request, CancellationToken cancellationToken)
+        {
+            var departments = await _departmentRepository.GetAllAsync();
+
+            return departments.Select(d => new GetAllDepartmentsResult
+            {
+                Id = d.Id,
+                Name = d.Name
+            }).ToList();
+        }
+    }
+}
---
+++ Application/Features/Handlers/DepartmentHandlers/GetDepartmentByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.Queries.DepartmentQueries;
+using Application.Features.Results.DepartmentResults;
+using Application.interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Handlers.DepartmentHandlers
+{
+    public class GetDepartmentByIdHandler : IRequestHandler<GetDepartmentByIdQuery, GetDepartmentByIdResult>
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public GetDepartmentByIdHandler(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public async Task<GetDepartmentByIdResult> Handle(GetDepartmentByIdQuery request, CancellationToken cancellationToken)
+        {
+            var department = await _departmentRepository.GetByIdAsync(request.Id);
+            if (department == null) return null;
+
+            return new GetDepartmentByIdResult
+            {
+                Id = department.Id,
+                Name = department.Name
+            };
+        }
+    }
+}
