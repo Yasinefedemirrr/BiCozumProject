d230504d544e2687ac7ef7aa84e433fbe637573b
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 11:40:04 +0300

    User Api Controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using Application.Features.Commands.UserCommands;
+using Application.Features.Queries.UserQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // Tüm kullanıcıları getir
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllUsersQuery());
+            return Ok(result);
+        }
+
+        // Id'ye göre kullanıcı getir
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetUserByIdQuery(id));
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        // Yeni kullanıcı oluştur
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            // Oluşturulan kullanıcıyı dön
+            var createdUser = await _mediator.Send(new GetUserByIdQuery(id));
+
+            return CreatedAtAction(nameof(GetById), new { id }, createdUser);
+        }
+
+        // Kullanıcı güncelle
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateUserCommand command)
+        {
+            if (id != command.Id)
+                return BadRequest();
+
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // Kullanıcı sil (Cascade Delete ile bağlı Complaints kayıtları da silinecek)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteUserCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
