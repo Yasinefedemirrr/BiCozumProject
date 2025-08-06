fc223477cb8e2f62516b311b6a21cd294bca055e
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 12:10:54 +0300

    Department Queries

---
+++ Application/Features/Queries/DepartmentQueries/GetDepartmentByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Results.DepartmentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.DepartmentQueries
+{
+    public class GetDepartmentByIdQuery : IRequest<GetDepartmentByIdResult>
+    {
+        public int Id { get; set; }
+        public GetDepartmentByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
