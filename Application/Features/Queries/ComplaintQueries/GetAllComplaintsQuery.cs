08b86531c318e50fba02d4f7b3c926c54295a630
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 10:17:16 +0300

    Complaint Queries

---
+++ Application/Features/Queries/ComplaintQueries/GetAllComplaintsQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Results.ComplaintResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.ComplaintQueries
+{
+    public class GetAllComplaintsQuery : IRequest<List<GetAllComplaintsResult>>
+    {
+    }
+}
---
+++ Application/Features/Queries/ComplaintQueries/GetComplaintByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Results.ComplaintResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.ComplaintQueries
+{
+    public class GetComplaintByIdQuery : IRequest<GetComplaintByIdResult>
+    {
+        public int Id { get; set; }
+
+        public GetComplaintByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
