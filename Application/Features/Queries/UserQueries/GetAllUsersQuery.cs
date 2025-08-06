75b8dc679bad983f1ae6bdf35d51a8ed8de63833
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-06 11:38:51 +0300

    User Query

---
+++ Application/Features/Queries/UserQueries/GetAllUsersQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Results.UserResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.UserQueries
+{
+    public class GetAllUsersQuery : IRequest<List<GetAllUsersResult>>
+    {
+    }
+}
---
+++ Application/Features/Queries/UserQueries/GetUserByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Results.UserResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.UserQueries
+{
+    public class GetUserByIdQuery : IRequest<GetUserByIdResult>
+    {
+        public int Id { get; set; }
+
+        public GetUserByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
