be60aa053dd5e36bd905f989a2e0f29b5a8a38c7
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-07 19:06:49 +0300

    CheckUser handler

---
+++ Application/Features/Queries/UserQueries/GetCheckUserQuery.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Results.AppUserResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queries.AppUserQueries
+{
+    public class GetCheckUserQuery : IRequest<GetCheckUserQueryResult>
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
