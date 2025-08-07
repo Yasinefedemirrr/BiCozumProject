8ec23ef4053b164c32caa9202eaa9dbdba2a8fb9
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-07 15:53:18 +0300

    Login , Token Dto

---
+++ Application/Dtos/TokenResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class TokenResponseDto
+    {
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpireDate { get; set; }
+
+        public TokenResponseDto(string token, DateTime expireDate, string refreshToken, DateTime refreshTokenExpireDate)
+        {
+            Token = token;
+            ExpireDate = expireDate;
+            RefreshToken = refreshToken;
+            RefreshTokenExpireDate = refreshTokenExpireDate;
+        }
+    }
+}
