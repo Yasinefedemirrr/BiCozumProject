a7f3180b8035d8e29885c1b065befc29b8eb0f89
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-07 15:54:07 +0300

    Token Default

---
+++ Application/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "http://localhost";
+        public const string ValidIssuer = "http://localhost";
+        public const string Key = "jwttokenjwttokenjwttokenjwttoken"; // 32+ karakter olmalı
+        public const int AccessTokenExpireMinutes = 5; // Access Token süresi
+        public const int RefreshTokenExpireDays = 7;   // Refresh Token süresi
+    }
+}
