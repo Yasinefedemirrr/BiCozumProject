a8cc36dcf3b3dc47523d32eff6ad56ec4aaba622
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-07 15:53:56 +0300

    AppUser Repository

---
+++ Persistance/Repositories/AppUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Application.interfaces;
+using Domain.Entity;
+using Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class AppUserRepository : IAppUserRepository
+    {
+        private readonly BiCozumContext _context;
+        public AppUserRepository(BiCozumContext context)
+        {
+            _context = context;
+        }
+        public Task<List<AppUser>> GetByFilterAsync(Expression<Func<AppUser, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
