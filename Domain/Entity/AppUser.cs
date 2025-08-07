b9f772b9b1676e5099545791315980ab3051911a
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-07 15:50:28 +0300

    AppUser Entity

---
+++ Domain/Entity/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class AppUser
+    {
+        public int AppUserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public int AppRoleId { get; set; }
+        public AppRole AppRole { get; set; }
+
+        public string RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpireDate { get; set; }
+    }
+}
